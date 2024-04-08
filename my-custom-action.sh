3c1caef673179156d5236ed30027005c6d7cc1dd
yaayala <105887914+yaayala@users.noreply.github.com>
2024-04-08 15:50:50 -0500

    feat: :construction: custom action and reusable worflow to build and publish docker image

--- my-custom-action.sh
+++ my-custom-action.sh
@@ -1,18 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
-inputpath=$1
+auditFile=$1
+auditOutput=$(cat $auditFile | grep -v 'npm')
 
+summary=":white_check_mark: All Good! No critical or moderate vulnerabilities found"
 
-check= $(cat "$inputpath" | grep -iE 'moderate|critical')
-echo $check
-# check=$( echo "$inputpath" | grep -iE 'moderate|critical')
-
-
-if [[ -z "$check" ]]; then
-message="ALL GOOD!"
-else
-message="ALERT!"
+if [[ $( (echo "$auditOutput" | grep 'moderate\|critical') ) ]]; then
+    summary=":fire: ALERT! Some vulnerabilites were found! Please check the report details!"
 fi
 
-echo $message
-echo "sum-audit=$message" >> $GITHUB_OUTPUT
+echo "$summary"
+echo "audit-summary=$summary" >> $GITHUB_OUTPUT
+
+
+# #!/bin/bash
+
+# inputpath=$1
+
+
+# check= $(cat "$inputpath" | grep -iE 'moderate|critical')
+# echo $check
+# # check=$( echo "$inputpath" | grep -iE 'moderate|critical')
+
+
+# if [[ -z "$check" ]]; then
+# message="ALL GOOD!"
+# else
+# message="ALERT!"
+# fi
+
+# echo $message
+# echo "sum-audit=$message" >> $GITHUB_OUTPUT
