64e4f699812a9a6784dcc1d0b53f765f5c2ebd97
yaayala <105887914+yaayala@users.noreply.github.com>
2024-04-08 16:34:05 -0500

    feat: :construction: custom action and reusable worflow to build and publish docker image

--- my-custom-action.sh
+++ my-custom-action.sh
@@ -2,16 +2,17 @@
 
 auditFile=$1
 auditOutput=$(cat $auditFile | grep -v 'npm')
-echo $auditOutput
 
 summary=":white_check_mark: All Good! No critical or moderate vulnerabilities found"
+operation=$(echo $auditOutput | grep -iE "moderate|critical")
+echo $operation
 
-if [ $(echo "$auditOutput" | grep -iE "moderate|critical") ]; then
-    summary=":fire: ALERT! Some vulnerabilites were found! Please check the report details!"
-fi
+# if [ $(echo "$auditOutput" | grep -iE "moderate|critical") ]; then
+#     summary=":fire: ALERT! Some vulnerabilites were found! Please check the report details!"
+# fi
 
-echo "$summary"
-echo "audit-summary=$summary" >> $GITHUB_OUTPUT
+# echo "$summary"
+# echo "audit-summary=$summary" >> $GITHUB_OUTPUT
 
 
 # #!/bin/bash
